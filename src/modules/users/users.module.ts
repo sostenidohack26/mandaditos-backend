6b6ea8e730e0d390073cb42b4ef7d006abe86af2
Sostenidohack26 <tucorreo@gmail.com>
2026-04-06 16:49:39 -0600

    test users controller routes

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { User } from './entities/user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { User } from './entities/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService],
-  exports: [TypeOrmModule, UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
